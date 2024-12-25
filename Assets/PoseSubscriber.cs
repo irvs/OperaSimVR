c59787e2a30752417f5ac308ea9d8b6fcb2566fe
akinari <akinari.kota.984@s.kyushu-u.ac.jp>
2024-12-25 22:56:07 +0900

    241225_23

--- Assets/PoseSubscriber.cs
+++ Assets/PoseSubscriber.cs
@@ -64,6 +64,24 @@ public class PoseSubscriber : MonoBehaviour
 
     }
 
+    /*
+    void Update()
+    {
+        //Vector3 newPosition = new Vector3(((float)21420.911) - offset_x, ((float)68.5) - offset_y, ((float)14027.59 - offset_z));
+        Vector3 newPosition = new Vector3(((float)21486.702) - ((float)21395.18), ((float)68.02) - 62, ((float)14065.63 - ((float)14034.45)));
+        //x:21395.18 y:,z:14034.45
+        Debug.Log(newPosition);
+
+        Quaternion newRotation = new((float)0.01826 * (-1), (float)0.342, (float)0.00078, (float)0.9395 * (-1));
+        rot_offset = new Vector3((float)rot_offset_x, (float)rot_offset_y, (float)rot_offset_z);
+        chenged_orientation = newRotation.eulerAngles - rot_offset;
+        
+
+        targetObject.transform.position = newPosition + new Vector3(-36f, 0, 52f); //GameObject.Find("map_Reference point").transform.position;// -new Vector3(-65,0,50);new Vector3(55.24f, 6.3f, 63.6f);//
+       targetObject.transform.eulerAngles = chenged_orientation;
+    }
+    */
+
     void Callback(PoseStampedMsg msg)
     {
         mode = GetComponent<mood_selector>();
@@ -72,7 +90,7 @@ public class PoseSubscriber : MonoBehaviour
         //Debug.Log(msg.pose.orientation);
         //
         //Vector3 newPosition = new Vector3(((float)msg.pose.position.y * (-1)+ offset_x), ((float)msg.pose.position.z)+offset_z, ((float)msg.pose.position.x)+ offset_y);
-        Vector3 newPosition = new Vector3(((float)msg.pose.position.x) + offset_y, ((float)msg.pose.position.z) + offset_z, (-(float)msg.pose.position.y * (-1) + offset_x));
+        Vector3 newPosition = new Vector3(((float)msg.pose.position.x) + offset_y, ((float)msg.pose.position.z) + offset_z, ((float)msg.pose.position.y + offset_x));
         Quaternion newRotation = new((float)msg.pose.orientation.y * (-1), (float)msg.pose.orientation.z, (float)msg.pose.orientation.x, (float)msg.pose.orientation.w * (-1));
         rot_offset = new Vector3((float)rot_offset_x, (float)rot_offset_y, (float)rot_offset_z);
         chenged_orientation = newRotation.eulerAngles - rot_offset;
@@ -95,7 +113,8 @@ public class PoseSubscriber : MonoBehaviour
             //Debug.Log(msg.pose.orientation);
             //
             //Vector3 newPosition = new Vector3(((float)msg.pose.position.y * (-1)+ offset_x), ((float)msg.pose.position.z)+offset_z, ((float)msg.pose.position.x)+ offset_y);
-            Vector3 newPosition = new Vector3(((float)msg.pose.position.y) + offset_x, ((float)msg.pose.position.z) + offset_z, (-(float)msg.pose.position.x * (-1) + offset_y));
+            Vector3 newPosition = new Vector3(((float)msg.pose.position.y) - ((float)21395.18), ((float)msg.pose.position.z) - 62, ((float)msg.pose.position.x - ((float)14034.45)));
+            // Vector3 newPosition = new Vector3(((float)14027) - offset_x, ((float)68.5) - offset_z, ((float)21420.9 - offset_y));
             Quaternion newRotation = new((float)msg.pose.orientation.y * (-1), (float)msg.pose.orientation.z, (float)msg.pose.orientation.x, (float)msg.pose.orientation.w * (-1));
             rot_offset = new Vector3((float)rot_offset_x, (float)rot_offset_y, (float)rot_offset_z);
             chenged_orientation = newRotation.eulerAngles - rot_offset;
@@ -105,7 +124,7 @@ public class PoseSubscriber : MonoBehaviour
             if (chenge_position_sw == true)
             {
                 //GameObject.Find("ic120").GetComponent<CharacterController>().enabled = false;
-                targetObject.transform.position = newPosition + new Vector3(-35.6f, 0, 51.6f); //GameObject.Find("map_Reference point").transform.position;// -new Vector3(-65,0,50);new Vector3(55.24f, 6.3f, 63.6f);//
+                targetObject.transform.position = newPosition + new Vector3(-36f, 0, 52f); //GameObject.Find("map_Reference point").transform.position;// -new Vector3(-65,0,50);new Vector3(55.24f, 6.3f, 63.6f);//
             }
             targetObject.transform.eulerAngles = chenged_orientation;
         }
